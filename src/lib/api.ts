15eacc809e641766b60c031939bc0700401df243
singsangssong <plmko0914@gmail.com>
2026-01-01 14:41:10 +0900

    FIX: Change api base url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://prod.api.studio-edge.app';
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://prod.api.studio-edge.app';
+// const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
